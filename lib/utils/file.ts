bcf5649a890ea5909e2528675f968d0895dd9293
Zack Ream <zbream@outlook.com>
2019-11-10 20:21:59 -0500

    fix: re-add yargs dependency
    BREAKING CHANGE: Drops support for Node <10.0.0

--- lib/utils/file.ts
+++ lib/utils/file.ts
@@ -1,41 +1,25 @@
-import * as fs from 'fs';
+import { promises as fsp } from 'fs';
+
+const encoding = 'utf8';
 
 export function fileCopy(fromPath: string, toPath: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    return fs.copyFile(fromPath, toPath, err => {
-      if (err) { return reject(err); }
-      return resolve();
-    });
-  });
+  return fsp.copyFile(fromPath, toPath);
 }
 
 export function fileRename(fromPath: string, toPath: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    return fs.rename(fromPath, toPath, err => {
-      if (err) { return reject(err); }
-      return resolve();
-    });
-  });
+  return fsp.rename(fromPath, toPath);
 }
 
 export function fileRead(path: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    return fs.readFile(path, 'utf8', (err, data) => {
-      if (err) { return reject(err); }
-      return resolve(data);
-    });
-  });
+  return fsp.readFile(path, encoding);
 }
 
 export function fileWrite(path: string, data: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    return fs.writeFile(path, data, 'utf8', err => {
-      if (err) { return reject(err); }
-      return resolve();
-    });
-  });
+  return fsp.writeFile(path, data, encoding);
 }
 
-export function fileBackup(path: string): Promise<void> {
-  return fileRename(path, `${path}.OLD`).catch(() => {});
+export async function fileBackup(path: string): Promise<void> {
+  try {
+    return await fileRename(path, `${path}.OLD`);
+  } catch {}
 }
